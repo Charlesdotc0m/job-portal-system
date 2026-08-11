a5df86ca507e5de14ab3e67beb8a4cf78706b2eb
selvin <selvincharles85@gmail.com>
2026-08-12 03:58:28 +0530

    Fix allowed hosts for Render

--- jobportal/jobportal/settings.py
+++ jobportal/jobportal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q**@8unqq=oszht01qe$eyu@rj(l5-c@4j4pl40tq*#%ptz*i&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
